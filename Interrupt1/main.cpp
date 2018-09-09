a57dbdd6d437aeddd15e92fe7625ec931fc59aee
Vinicius Souza <viniciusls22@gmail.com>
2018-09-09 14:53:47 -0300

    Complete Interrupt 1 and Half Interrupt 2
    Signed-off-by: Vinicius Souza <viniciusls22@gmail.com>

---
+++ Interrupt1/main.cpp
@@ -0,0 +1,103 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+
+#define BAUD 9600
+#define MYUBRR_VALUE F_CPU/16/BAUD-1
+
+unsigned int debounce_limit = 200;
+unsigned int time_last_interrupt = 0;
+bool led_state = 0;
+
+unsigned long ms = 0;
+unsigned char fract = 0;
+unsigned long overflow = 0;
+
+void uart_init() {
+	// Set baud rate
+    UBRR0 = MYUBRR_VALUE;
+
+    // Set TX and RX
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+
+    // Set frame format: 8 data, 1 stop bit (00000110)
+    UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
+}
+
+void uart_tx(char *data) {
+    for (int i=0;data[i]!=0;i++) {
+        while ( !(UCSR0A & (1 << UDRE0)) );
+        UDR0 = data[i];
+    }
+}
+
+void clock_init(){
+	TCCR0B |= (1 << CS00);
+	TCCR0B |= (1 << CS01);
+	TIMSK0 |= (1 << TOIE0);
+}
+
+ISR(TIMER0_OVF_vect) {
+	unsigned long m = ms;
+	unsigned char f = fract;
+
+	m += 1;
+	f += 3;
+	if (f >= 125) {
+		f -= 125;
+		m += 1;
+	}
+	fract = f;
+	ms = m;
+	overflow++;
+}
+
+unsigned long millis() {
+	unsigned long m;
+	m = ms;
+
+	return m;
+}
+
+int debounce() {
+  if ( (millis() - time_last_interrupt) > debounce_limit) {
+    time_last_interrupt = millis();
+    return 1;
+  } else return 0;
+}
+
+ISR(INT4_vect) {
+	uint8_t oldSREG = SREG;
+	if (debounce()) {
+		uart_tx("Change LED\n");
+		led_state = !led_state;
+	}
+	SREG = oldSREG;
+}
+
+int main(void) {
+	cli();
+	uart_init();
+	clock_init();
+
+	DDRE &= ~(1 << DDE4);				// Set 0 to DDRE4 (PE4)
+	DDRH |= (1 << DDH5);				// Set 1 to DDRH5 (PH5)
+
+	EICRA = 0x00;
+	EIMSK |= (1 << INT4);
+	sei();
+
+	while (1) {
+
+		if (led_state) {
+			PORTH |= (1 << PH5);
+		} else {
+			PORTH &= ~(1 << PH5);
+		}
+
+	}
+
+}
