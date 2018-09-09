2a1cb185c08f3afa0989a7c23f245427e07b2a8e
Vinicius Souza <viniciusls22@gmail.com>
2018-09-09 14:47:17 -0300

    ADC, Interrupt P1 and half Interrupt P2
    Signed-off-by: Vinicius Souza <viniciusls22@gmail.com>

--- ADC/main.cpp
+++ ADC/main.cpp
@@ -1,16 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define BAUD 9600
 #define MYUBRR_VALUE F_CPU/16/BAUD-1
 
-int dig_to_analog(float dig){
-  return dig*(1024.0/5.0);
-
-}
-
-int analog_to_dig(float analog){
-  return analog*(1024.0/5.0);
+void adc_init() {
+    ADMUX = (1 << REFS0);
+    ADCSRA = (1 << ADEN);
+    //ADCSRA = (1 << ADIF);
 }
 
 void uart_init() {
@@ -24,17 +24,37 @@ void uart_init() {
     UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
 }
 
-void adc_init() {
-	ADMUX = 0x00;
-	ADCSRA = (1 << ADEN);
+void uart_tx(char *data) {
+    for (int i=0;data[i]!=0;i++) {
+        while ( !(UCSR0A & (1 << UDRE0)) );
+        UDR0 = data[i];
+    }
+}
+
+int dig_to_analog(float dig){
+  return (dig*5)/1023;
+
+}
+
+int analog_to_dig(float analog){
+  return (analog*1023)/5;
 }
 
 int analog_read() {
 	int val;
 	ADCSRA |= (1 << ADSC);
+	while ( !(ADCSRA & (1 << ADIF)) );
 	val = ADC;
 	ADCSRA &= ~(1 << ADSC);
 	return val;
+}
+
+int rms_read(int repeat) {
+	float accumulated = 0;
+	for(int n=0;n<repeat;n++) {
+		accumulated += analog_read();
+	}
+	return accumulated/repeat;
 }
 
 int main(void) {
@@ -42,8 +62,25 @@ int main(void) {
 	adc_init();
 
 	while (1) {
-		_delay_ms(5000);
-		int analog = analog_read();
+		_delay_ms(1000);
+		char dig[100];
+		char anal[100];
+		memset(dig, 0, sizeof(dig));
+		memset(anal, 0, sizeof(anal));
+
+		int digital = rms_read(100);
+		int analog = dig_to_analog(digital);
+
+		itoa(digital, dig, 10);
+		itoa(analog, anal, 10);
+
+		strcat(dig, "\n");
+		strcat(anal, "\n");
+
+		uart_tx(dig);
+		uart_tx(anal);
+		uart_tx("==========\n");
+
 	}
 
 }
